7a1d292673797d0ac7843e6fb9376a3a806a124d
Artem Kovalevskyi <kovalevskyi.artem@gmail.com>
2025-02-05 12:28:13 +0100

    docs

--- api.py
+++ api.py
@@ -1,24 +1,55 @@
 import requests
-import json
 
 class Scraper():
+    """
+    A class to scrape bus data from the LPP API.
+    """
+
     def __init__(self):
+        """
+        Initializes the API client with a base URL and a session.
+        Attributes:
+            base_url (str): The base URL for the API.
+            session (requests.Session): The session object for making HTTP requests.
+        """
         self.base_url = "https://www.lpp.si/lpp/ajax/1/"
         self.session = requests.Session()
 
-    def get_line(self, line):
+    def get_line(self, line: str) -> dict:
+        """
+        Fetches data for a specific bus line.
+        Args:
+            line (str): The bus line identifier.
+        Returns:
+            dict: The JSON response from the API containing the bus line data.
+        """
         url = self.base_url + line
         response = self.session.get(url)
         return response.json()
     
-    def all_buses(self, line):
+    def all_buses(self, line: str) -> list:
+        """
+        Retrieve all bus keys for a given line.
+        Args:
+            line (str): The bus line identifier.
+        Returns:
+            list: A list of bus keys for the specified line.
+        """
         data = self.get_line(line)
         buses = []
         for bus in data:
             buses.append(bus[0]['key'])
         return buses
     
-    def filter_line(self, line, key: int | str | list):
+    def filter_line(self, line: str, key: int | str | list) -> list:
+        """
+        Filters the bus data based on the provided key.
+        Args:
+            line (str): The line identifier to fetch the bus data.
+            key (int | str | list): The key to filter the bus data. It can be an integer, string, or list of integers/strings.
+        Returns:
+            list: A list of buses that match the provided key.
+        """
         data = self.get_line(line)
 
         try:
--- helper.py
+++ helper.py
@@ -4,7 +4,18 @@ from difflib import get_close_matches
 with open("lines.json", "r") as f:
     lines = json.load(f)
 
-def match_stop(stop: str):
+def match_stop(stop: str) -> dict | None:
+    """
+    Finds and returns the first matching bus stop from a list of bus stops.
+    The function performs a case-insensitive search for the provided stop name
+    within a list of bus stops. If no exact matches are found, it attempts to
+    find close matches using the `get_close_matches` function.
+    Args:
+        stop (str): The name of the bus stop to search for.
+    Returns:
+        dict or None: The first matching bus stop as a dictionary if found,
+                      otherwise None.
+    """
     stop = stop.lower()
     matches = [line for line in lines if stop in line['name'].lower()]
     if not matches:
@@ -13,17 +24,37 @@ def match_stop(stop: str):
             matches = [line for line in lines if line['name'].lower() in close_matches]
     return matches[0] if matches else None
 
-def get_line_id(stop, direction):
+def get_line_id(stop: str, direction: str) -> str | None:
+    """
+    Retrieve the line ID for a given stop and direction.
+    Args:
+        stop (str): The name of the bus stop.
+        direction (str): The direction of the bus line.
+    Returns:
+        str | None: The ID of the bus line if found, otherwise None.
+    """
     for line in lines:
         if line['name'] == stop['name'] and line['direction'] == direction:
             return line['id']
     return None
 
-def load_user_data():
+def load_user_data() -> dict:
+    """
+    Loads user data from a JSON file.
+    Returns:
+        dict: A dictionary containing the user data loaded from 'userdata.json'.
+    """
     with open("userdata.json", "r") as f:
         return json.load(f)
 
-def save_user_data(data):
+def save_user_data(data) -> None:
+    """
+    Save user data to a JSON file.
+    Args:
+        data (dict): The user data to be saved.
+    Returns:
+        None
+    """
     with open("userdata.json", "w") as f:
         json.dump(data, f)
 
